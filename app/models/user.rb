0ce7288f5b50b123d78d1d569c506e5a0cf73585
Miguel Angel Ibarra, Jr <miggs.ibarra@gmail.com>
2018-01-23 21:08:54 -0800

    creates db, migration and model for the user model

---
+++ app/models/user.rb
@@ -0,0 +1,25 @@
+class User < ApplicationRecord
+  require 'bcrypt'
+  include BCrypt
+
+  validates :first_name, :last_name, :email, :password_hash, presence: true
+  validates :email, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Not proper email format'}
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  def self.login(email, password)
+    user = User.find_by_email(email)
+    if user.password == password
+      return user
+    else
+      nil
+    end
+  end
+end
