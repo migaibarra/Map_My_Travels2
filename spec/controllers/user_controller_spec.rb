a33e3eced44a14ef2c2aec3ecca918b1b7479cfd
Miguel Angel Ibarra, Jr <miggs.ibarra@gmail.com>
2018-01-26 15:01:29 -0800

    adds and tests the create route in rails

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -11,4 +11,18 @@ RSpec.describe UsersController, type: :controller do
       expect(response).to render_template(:new)
     end
   end
+
+  describe "POST #create" do
+    context "with valid attributes" do
+      it 'saves the new user in the database' do
+        expect { post :create, params: { user: { first_name: "Bob", last_name: "Jenkins", email: "bjenkins@example.com", password: "Pa$$w0rd" } } }
+        .to change{ User.count }.by(1)
+      end
+      it 'redirects to another page (will eventually be the homepage)' do
+        post :create, params: { user: { first_name: "Bob", last_name: "Jenkins", email: "bjenkins@example.com", password: "Pa$$w0rd" } }
+        expect(response.status).to eq(302)
+      end
+
+    end
+  end
 end
